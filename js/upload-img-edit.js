8a2751a8f66361eccc07c164bee2057372e4b66f
gaofengoo <1557531860@qq.com>
2018-12-28 15:50:12 +0800

    Update JavaScriptCode (1228)

---
+++ js/upload-img-edit.js
@@ -0,0 +1,40 @@
+var imgFile = new ImgUploadeFiles('.box',function(e){
+	this.init({
+		MAX : 3, //限制个数
+		MH : 5800, //像素限制高度
+		MW : 5900, //像素限制宽度
+		callback : function(arr){
+			console.log(arr)
+		}
+	});
+});
+var imgFile1 = new ImgUploadeFiles('.box2',function(e){
+	this.init({
+		MAX : 5,
+		MH : 1800, //像素限制高度
+		MW : 1900, //像素限制宽度
+		callback : function(arr){
+			console.log(arr)
+		}
+	});
+});
+var imgFile3 = new ImgUploadeFiles('.box3',function(e){
+	this.init({
+		MAX : 3,
+		MH : 800, //像素限制高度
+		MW : 900, //像素限制宽度
+		callback : function(arr){
+			console.log(arr)
+		}
+	});
+});
+var imgFile4 = new ImgUploadeFiles('.box4',function(e){
+	this.init({
+		MAX : 1,
+		MH : 800, //像素限制高度
+		MW : 900, //像素限制宽度
+		callback : function(arr){
+			console.log(arr)
+		}
+	});
+});
