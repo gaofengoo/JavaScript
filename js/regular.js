5ae9ffb3a371c98db8d233b63487b55303362d40
gaofengoo <1557531860@qq.com>
2019-01-22 22:24:39 +0800

    Update JavaScript

---
+++ js/regular.js
@@ -0,0 +1,8 @@
+// var testValue = "My name is gaofeng";
+// var subsValue = "gaofeng";
+// var iValue = testValue(subsValue);
+// console.log(iValue);
+
+var regExp = /regular expression/;
+
+var re1 = /name\s+gaofeng/;
