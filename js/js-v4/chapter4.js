dc066b6d106f2a210c9bc2d5135074d20a79df63
huanggaofeng <15879831437@163.com>
2023-05-06 14:04:07 +0800

    Revert "chapter4.js更新验证"
    This reverts commit 7aad10f7192ca8fb2d6ee8dd039321e334dee8a8.

--- js/js-v4/chapter4.js
+++ js/js-v4/chapter4.js
@@ -1,17 +1,5 @@
-'use strict';
-
 let reg1 = /a/;
 let f1 = function () {}
 console.log(typeof reg1, typeof f1);
 
-function f2 () {
-	var a = 'a';
-	let b = 'b';
-}
-// console.log(a, b);
-
-console.log(auto1);
-auto1 = 3;
-console.log(auto1);
-var auto1;
-
+function f2 () {}
